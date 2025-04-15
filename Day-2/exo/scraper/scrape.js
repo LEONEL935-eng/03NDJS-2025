002d9c0ff7edadd32ec8d7d8bfb182b195e9a037
root <root@DESKTOP-SPR4OAH.>
2025-04-15 19:05:34 +0200

    Ajout du fichier output.json

--- Day-2/exo/scraper/scrape.js
+++ Day-2/exo/scraper/scrape.js
@@ -1,3 +1,4 @@
+import * as fs from 'fs';
 import * as cheerio from 'cheerio';
 
 const $ = await cheerio.fromURL('https://www.css.cnrs.fr/scrape/nobel_all.html');
@@ -9,12 +10,16 @@ $('table tbody tr').each((i, row) => {
 
   const record = {
     année: $(cols[0]).text().trim(),
-    discipline: $(cols[1]).text().trim(),
-    nom: $(cols[2]).text().trim(),
-    pysiologie: $(cols[3]).text().trim(),
+    phisic: $(cols[1]).text().trim(),
+    chimie: $(cols[2]).text().trim(),
+    physio: $(cols[3]).text().trim(),
+    Litterature: $(cols[4]).text().trim(),
+    Paix: $(cols[5]).text().trim(),
+    Economie: $(cols[6]).text().trim(),
   };
 
   nobels.push(record);
 });
 
-console.log(nobels);
+fs.writeFileSync('output.json', JSON.stringify(nobels, null, 2), "utf-8")
+console.log("fichier créer avec succès");
