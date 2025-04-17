e3deaa7fba45fb28f4d412069bb0432ceeb940a1
root <root@DESKTOP-SPR4OAH.>
2025-04-17 23:04:57 +0200

    ajout

--- Day-4/project-EXAMEN/auth-api/serveur.js
+++ Day-4/project-EXAMEN/auth-api/serveur.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
 
 app.use(express.json());
-app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
-app.listen(3000, () => {
-    console.log('Serveur running on http://localhost:3000');
-})
-require('dotenv').config();
+
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/users', require('./routes/users'));
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Erreur serveur' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Serveur démarré sur le port ${PORT}`));
