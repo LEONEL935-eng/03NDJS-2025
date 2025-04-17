3c41e3eb7b17c2c14debe88315079b853b6f4cb8
root <root@DESKTOP-SPR4OAH.>
2025-04-17 22:12:44 +0200

    stockage en memoire

--- Day-4/project-EXAMEN/auth-api/models/userModel.js
+++ Day-4/project-EXAMEN/auth-api/models/userModel.js
@@ -1,3 +1,24 @@
 const users = [];
+let idCounter = 1;
 
-module.exports = users;
+module.exports = {
+  create: (user) => {
+    const newUser = { id: idCounter++, ...user };
+    users.push(newUser);
+    return newUser;
+  },
+  
+  findByEmail: (email) => users.find(user => user.email === email),
+  
+  findById: (id) => users.find(user => user.id === id),
+  
+  getAll: () => users.map(user => ({ id: user.id, email: user.email })),
+  
+  delete: (id) => {
+    const index = users.findIndex(user => user.id === id);
+    if (index !== -1) {
+      return users.splice(index, 1)[0];
+    }
+    return null;
+  }
+};
